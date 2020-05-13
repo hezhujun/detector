ec25e6fbb526c84a8e141ce8ea17023ff2c40050
hezhujun <1214507717@qq.com>
2020-05-13 10:23:40 +0800

    增加SummaryWriterWrap和画bounding box的辅助函数

--- lib/util.py
+++ lib/util.py
@@ -1,4 +1,6 @@
 import torch.optim.lr_scheduler as lr_scheduler
+from torch.utils.tensorboard import SummaryWriter
+from PIL import ImageDraw
 
 
 class WarmingUpScheduler(lr_scheduler.LambdaLR):
@@ -12,3 +14,45 @@ class WarmingUpScheduler(lr_scheduler.LambdaLR):
             else:
                 return 1
         super(WarmingUpScheduler, self).__init__(optimizer, f)
+
+
+class SummaryWriterWrap(SummaryWriter):
+    def __init__(self, log_dir=None, comment='', purge_step=None, max_queue=10,
+                 flush_secs=120, filename_suffix=''):
+        super(SummaryWriterWrap, self).__init__(log_dir=log_dir,
+                                                comment=comment,
+                                                purge_step=purge_step,
+                                                max_queue=max_queue,
+                                                flush_secs=flush_secs,
+                                                filename_suffix=filename_suffix)
+        self.cur_epoch = 0
+
+    def step(self, epoch=None):
+        if epoch is None:
+            self.cur_epoch += 1
+        else:
+            self.cur_epoch = epoch
+
+    def add_scalar(self, tag, scalar_value, global_step=None, walltime=None):
+        step = global_step if global_step else self.cur_epoch
+        super(SummaryWriterWrap, self).add_scalar(tag, scalar_value, global_step=step, walltime=walltime)
+
+
+def draw_image(image, boxes, labels=None, scores=None):
+    """
+
+    :param image: PIL.Image.Image
+    :param boxes: numpy ndarray (N, 4): (x1, y1, x2, y2)
+    :param labels: numpy ndarray (N,)
+    :param scores: numpy ndarray (N,)
+    :return:
+    """
+    draw = ImageDraw.Draw(image, mode="RGBA")
+    N = len(boxes)
+    for i in range(N):
+        x1, y1, x2, y2 = boxes[i]
+        #x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        xy = [(x1, y1), (x2, y2)]
+        draw.rectangle(xy, fill=(0, 0, 0, 0), outline=(0xff, 0, 0, 0xff))
+
+    return image
