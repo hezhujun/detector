c25582699ff39436a52e2b677c0271a96e2487f2
hezhujun <1214507717@qq.com>
2020-05-14 19:05:14 +0800

    完善WarmingUpScheduler

--- lib/util.py
+++ lib/util.py
@@ -8,12 +8,17 @@ class WarmingUpScheduler(lr_scheduler.LambdaLR):
     def __init__(self, optimizer, init_factor=0.1, steps=100):
         assert init_factor >= 0 and init_factor <= 1
         assert steps > 1
+        self.steps = steps
         def f(epochs):
             if epochs < steps:
                 return (1 - init_factor) / (steps - 1) * epochs + init_factor
             else:
                 return 1
         super(WarmingUpScheduler, self).__init__(optimizer, f)
+        
+    def step(self, epoch=None):
+        if epoch and epoch < self.steps or self.last_epoch + 1 < self.steps:
+            super(WarmingUpScheduler, self).step(epoch)
 
 
 class SummaryWriterWrap(SummaryWriter):
